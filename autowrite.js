8c6a973d69bb54060cc21d5f0c1a2ba9515107b4
Kevin <kevinalexanderduran8@gmail.com>
2021-02-08 05:23:58 -0800

    AUTOTYPE FINISHED

--- autowrite.js
+++ autowrite.js
@@ -1,2 +1,23 @@
-//take input from form
-//display letters one at a time to mimic typing
+let output = document.querySelector('.output');
+let myInput = document.querySelector('.myInput');
+let button = document.querySelector('button');
+let index = 0;
+
+button.addEventListener('click', () => {
+    setInterval(autoType, 100);
+});
+
+function autoType() {
+    let auto = "hello " + myInput.value+ ',' + " how can I be of assistance?";
+    output.innerText = auto.slice(0,index);
+    index++;
+    if(index > auto.length){
+        return;
+    }
+   
+}
+
+
+
+
+

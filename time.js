eec92c0aba4f5c8abb4aec32ecaae8bf5acdd553
Kevin <kevinalexanderduran8@gmail.com>
2021-04-21 08:33:54 -0700

    FINISHED

--- time.js
+++ time.js
@@ -2,6 +2,8 @@ let time  = document.querySelector('.time_button');
 let averageLifespan = 28470; //78 years is average human life expectancy
 let age = document.querySelector('input');
 let output = document.querySelector('.output');
+let days = document.querySelector('.days')
+
 
 time.addEventListener('click', () => {
    deathClock();
@@ -9,9 +11,13 @@ time.addEventListener('click', () => {
 
 function deathClock() {
     var result = (averageLifespan - (age.value*365)).toString();
-    output.innerHTML ='<div class="death">you have less than </div> <div class="death">' + result + '</div> <div class="death"> days left alive</div>';
+    days.innerText = result;
+    output.style.opacity = '1';
 }
 
 
-
+age.addEventListener('click', () => {
+  age.value = '';
+  output.style.opacity = '0';
+})
 

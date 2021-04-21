349149f413ef810dcd25489d577a8b72643f46e1
Kevin <kevinalexanderduran8@gmail.com>
2021-04-21 05:21:39 -0700

    FINISHED: all bugs fixed

--- autowrite.js
+++ autowrite.js
@@ -3,22 +3,22 @@ let myInput = document.querySelector('.myInput');
 let button = document.querySelector('button');
 let index = 0;
 
+
 button.addEventListener('click', () => {
-    setInterval(autoType, 100);
+    var test = setInterval(function(){
+            var auto = "hello " + myInput.value ;
+            output.innerText = auto.slice(0,index);
+            index++;
+            if(index > auto.length){ 
+                clearInterval(test);
+            }
+    }, 100);
 });
+myInput.addEventListener('click',function(){
+   if(output.innerText !== ''){
+        myInput.value = '';
+        output.innerText = '';
+        index = 0;
+   }
+})
 
-function autoType() {
-    let auto = "hello " + myInput.value+ ',' + " how can I be of assistance?";
-    output.innerText = auto.slice(0,index);
-    index++;
-    if(index > auto.length){
-        return;
-    }
-   
-};
- 
-
-
-
-
-l

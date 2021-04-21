a968a00afa448e35f0aec32e9e45d3664bac1468
Kevin <kevinalexanderduran8@gmail.com>
2021-04-20 21:11:16 -0700

    COMPLETE: styling completed. functionality achieved

---
+++ dark.js
@@ -0,0 +1,12 @@
+let dark = document.querySelector('input');
+dark.addEventListener('click',function(){
+    if(this.checked){
+        document.querySelector('body').style.backgroundColor = "black";
+        document.querySelector('h2').style.color = "rgb(17, 16, 16)"
+        document.querySelector('.container').style.backgroundColor = "#ccc"
+    }else{
+        document.querySelector('.container').style.backgroundColor = "rgb(17, 16, 16)"
+        document.querySelector('body').style.backgroundColor = "#ccc";
+        document.querySelector('h2').style.color = "rgb(231, 43, 43)";
+    }
+})

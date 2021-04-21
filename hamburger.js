d6318e8b02183bafd4c8e9cf05b6e45c6cbfc3c4
Kevin <kevinalexanderduran8@gmail.com>
2021-04-20 21:45:08 -0700

    test

--- hamburger.js
+++ hamburger.js
@@ -1,7 +1,13 @@
 let hamburger = document.querySelector('.hamburger');
 let nav = document.querySelector('.nav');
+let lineRotateOne = document.querySelector('.lineOne');
+let lineRotateTwo = document.querySelector('.lineThree');
+let lineRotateMiddle = document.querySelector('.lineTwo');
 
 hamburger.addEventListener('click', () => {
     nav.classList.toggle('visible');
+    lineRotateOne.classList.add('navRotateOne')
+    lineRotateTwo.classList.add('navRotateTwo')
+    lineRotateMiddle.classList.add('navRotateMiddle');
 })
 

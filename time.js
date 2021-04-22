2daa2015868eb8abd2b13229f16c44af9d7cfc73
Kevin <kevinalexanderduran8@gmail.com>
2021-04-22 04:14:30 -0700

    FUNCTIONALITY: added bottom text on delay

--- time.js
+++ time.js
@@ -2,22 +2,25 @@ let time  = document.querySelector('.time_button');
 let averageLifespan = 28470; //78 years is average human life expectancy
 let age = document.querySelector('input');
 let output = document.querySelector('.output');
-let days = document.querySelector('.days')
+let days = document.querySelector('.days');
+let wisdom = document.querySelector('.wisdom');
 
 
 time.addEventListener('click', () => {
    deathClock();
 });
 
+age.addEventListener('click', () => {
+    age.value = '';
+    output.style.opacity = '0'
+    wisdom.style.opacity = '0'
+  });
+
 function deathClock() {
-    var result = (averageLifespan - (age.value*365)).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");//the replace was from stack overflow
+    var result = (averageLifespan - (age.value*365)).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");//the replace was from stack overflow to add a comma
     days.innerText = result;
     output.style.opacity = '1';
+    wisdom.style.opacity = '1'
+
 }
-
-
-age.addEventListener('click', () => {
-  age.value = '';
-  output.style.opacity = '0'
-})
 

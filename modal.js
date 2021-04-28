f5d2761601080d6ec696137637451f39400bc0b8
Kevin <kevinalexanderduran8@gmail.com>
2021-04-28 08:31:29 -0700

    COMPLETE: MODAL API CALL COMPLETE

--- modal.js
+++ modal.js
@@ -2,7 +2,8 @@ let modalBtn = document.querySelector('.modalBtn');
 let modal = document.querySelector('.modal');
 let close = document.querySelector('.close');
 let api_key = 'e1e6bd2072ba2201d3f480a464b90e40';
-
+let outputTop = document.querySelector('.outputTop');
+let outputBottom = document.querySelector('.outputBottom');
 
 
 modalBtn.addEventListener('click', ()=>{
@@ -21,7 +22,11 @@ function getMovie(){
     console.log('random page : ' + randomPage)
     fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${api_key}&with_genres=27&page=${randomPage}&language=en-US`)
         .then(res => res.json())
-        .then(data => console.log(data.results[randomMovie].original_title))
+        .then(data =>{
+            outputTop.innerHTML = `<h2 class="outputTitle">${data.results[randomMovie].original_title}</h2><p class="outputDesc">${data.results[randomMovie].overview}</p>`;
+            outputBottom.innerHTML= `<img class="myImage" src="https://image.tmdb.org/t/p/original/${data.results[randomMovie].poster_path}">`
+            
+        })
 }
 
 

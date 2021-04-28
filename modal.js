f9546d3c87e7be547cb7b227eff5e5440127b37c
Kevin <kevinalexanderduran8@gmail.com>
2021-04-27 21:33:17 -0700

    HALFWAY SOLVED

--- modal.js
+++ modal.js
@@ -2,26 +2,32 @@ let modalBtn = document.querySelector('.modalBtn');
 let modal = document.querySelector('.modal');
 let close = document.querySelector('.close');
 let api_key = 'e1e6bd2072ba2201d3f480a464b90e40';
-let randomMovie = Math.floor(Math.random() * 20 );
-console.log(randomMovie)
+
+
 
 modalBtn.addEventListener('click', ()=>{
     modal.classList.add('active');
+    getMovie();
 });
 
 close.addEventListener('click', ()=>{
     modal.classList.remove('active');
 });
 
-fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${api_key}&with_genres=27`)
-.then(res => res.json())
-.then(data => console.log(data)
+function getMovie(){
+    let randomMovie = Math.floor(Math.random() * 20 );
+    let randomPage = Math.floor(Math.random()* 100);
+    console.log('random number : ' + randomMovie)
+    console.log('random page : ' + randomPage)
+    fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${api_key}&with_genres=27&page=${randomPage}&language=en-US`)
+        .then(res => res.json())
+        .then(data => console.log(data.results[randomMovie].original_title))
+}
 
-)
 
 
-//turn fetch into function
-
-//on click activate function
-
-//function takes input
+//todo:
+    //need to solve random page number query issue
+    //NEED TO FIND OUT HOW TO ACCESS PAGE NUMBER IN REQUEST
+    //need to display to dom
+    //need to display image as well

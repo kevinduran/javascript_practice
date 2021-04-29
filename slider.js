eb364f243050e09acee98cedda53179fb2852059
Kevin <kevinalexanderduran8@gmail.com>
2021-04-29 09:06:22 -0700

    test

--- slider.js
+++ slider.js
@@ -1,5 +1,28 @@
 let left = document.querySelector('.left');
 let right = document.querySelector('.right');
-let img = document.querySelector('.carousel_container');
+let img = document.querySelectorAll('.photo');
+let counter = 0;
 
+let test = document.querySelector('.carousel_container');
 
+left.addEventListener('click', () => {
+    counter++;
+    if(counter > 2){
+        counter = 0;
+    }
+    console.log(counter);
+    console.log(img[counter]);
+    test.style.transition = "1s ease all";
+    test.style.transform = "translateX(-360px)";
+})
+
+right.addEventListener('click', () => {
+    counter++;
+    if(counter > 2){
+        counter = 0;
+    }
+    console.log(counter);
+    console.log(img[counter]);
+    test.style.transition = "1s ease all";
+    test.style.transform = "translateX(360px)";
+})

6d4b1b4212282d0a0905fffdfd54506ea4a32fdc
Kevin <kevinalexanderduran8@gmail.com>
2021-04-30 05:56:28 -0700

    test

--- slider.js
+++ slider.js
@@ -3,19 +3,24 @@ let button__next = document.querySelector('.button__next');
 let counter = 0;
 
 button__prev.addEventListener('click', () => {
+    getPrev();
+})
+button__next.addEventListener('click', () => {
+    getNext();
+})
+
+function getPrev() {
     counter--;
     if(counter < 0){
         counter = 2;
     }
     console.log(counter);
-})
-button__next.addEventListener('click', () => {
+}
+
+function getNext() {
     counter++;
     if(counter > 2){
         counter = 0;
     }
     console.log(counter);
- 
-})
-
-
+}

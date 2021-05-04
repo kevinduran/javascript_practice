c47a84cdb19cd1e5587a45f2da6c313fda358a4f
Kevin <kevinalexanderduran8@gmail.com>
2021-05-04 05:00:00 -0700

    test

--- slider.js
+++ slider.js
@@ -1,20 +1,28 @@
 let button__prev = document.querySelector('.button__prev');
 let button__next = document.querySelector('.button__next');
+let image = document.querySelector('.photo__container--photo')
 let counter = 0;
 
-button__prev.addEventListener('click', () => {
+ button__prev.addEventListener('click', () => {
     getPrev();
 })
 button__next.addEventListener('click', () => {
     getNext();
 })
 
+let imageArray= [
+    'horror2.jpeg',
+    'horror3.jpeg',
+    'horror4.jpeg'
+]
+
+
 function getPrev() {
     counter--;
     if(counter < 0){
         counter = 2;
     }
-    console.log(counter);
+    image.src=`assets/${imageArray[counter]}`;
 }
 
 function getNext() {
@@ -22,5 +30,5 @@ function getNext() {
     if(counter > 2){
         counter = 0;
     }
-    console.log(counter);
+    image.src=`assets/${imageArray[counter]}`;
 }

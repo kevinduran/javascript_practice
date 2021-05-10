8fefe7a418cf60ecad45fd660b369c02ad3dfdff
Kevin <kevinalexanderduran8@gmail.com>
2021-05-10 05:34:23 -0700

    test

--- pokemon.js
+++ pokemon.js
@@ -14,6 +14,14 @@ kanyeButton.addEventListener('click', ()=>{
 
 })
 
-fetch('https://pokeapi.co/api/v2/egg-group/monster')
-.then()
-.then()
+
+
+// fetch('https://pokeapi.co/api/v2/pokemon')
+// .then(res => res.json())
+// .then(response=>console.log(response))
+
+//this gets every pokemon they have.
+//BTW to get a description, we need to append the resource_uri to the end of our fetch url
+
+//what we want:
+    //-a pokedex that only displays ghost pokemon (a spookadex)

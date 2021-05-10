39951f58eff9f7b799cd5d99181138e147140ea5
Kevin <kevinalexanderduran8@gmail.com>
2021-05-10 09:24:49 -0700

    test

--- pokemon.js
+++ pokemon.js
@@ -1,20 +1,3 @@
-let kanyeOutput = document.querySelector('.output');
-
-let kanyeButton = document.querySelector('button');
-
-kanyeButton.addEventListener('click', ()=>{
-    let kanyeInput= document.querySelector('input').value;
-    fetch(`https://pokeapi.co/api/v2/pokemon/${kanyeInput}`)
-    .then(kanyeResult=>kanyeResult.json())
-    .then(kanyeRes=>{
-        console.log('pokemon : ' + kanyeRes.forms[0].name)
-        console.log('move : ' + kanyeRes.moves[0].move.name)
-        kanyeOutput.innerHTML = `<img src=${kanyeRes.sprites.other.dream_world.front_default} />`
-    })
-
-})
-
-
 
 // fetch('https://pokeapi.co/api/v2/pokemon')
 // .then(res => res.json())

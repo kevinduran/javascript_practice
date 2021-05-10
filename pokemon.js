f3aeefb18916feba1f75d00c331a5e587a7178a3
Kevin <kevinalexanderduran8@gmail.com>
2021-05-10 09:39:15 -0700

    test

--- pokemon.js
+++ pokemon.js
@@ -1,10 +1,6 @@
 
-// fetch('https://pokeapi.co/api/v2/pokemon')
-// .then(res => res.json())
-// .then(response=>console.log(response))
+ fetch('https://pokeapi.co/api/v2/pokemon/gengar')
+ .then(res => res.json())
+ .then(response=>console.log(response))
 
-//this gets every pokemon they have.
-//BTW to get a description, we need to append the resource_uri to the end of our fetch url
-
-//what we want:
-    //-a pokedex that only displays ghost pokemon (a spookadex)
+//to get a description, we need to append the resource_uri to the end of our fetch url

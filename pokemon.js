8573b35d4ed84efd19946cb6ff2b39b9584014d4
Kevin <kevinalexanderduran8@gmail.com>
2021-05-10 21:52:48 -0700

    test

--- pokemon.js
+++ pokemon.js
@@ -1,6 +1,32 @@
+let pokemonImage = document.querySelector('.pokemon--image');
+let pokemonDesc = document.querySelector('.pokemon--desc');
+let pokedexStart = document.querySelector('.pokedex--start');
 
- fetch('https://pokeapi.co/api/v2/pokemon/gengar')
- .then(res => res.json())
- .then(response=>console.log(response))
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+  }
+
+
+pokedexStart.addEventListener('click',()=>{
+    getRandomInt(76);
+    fetch(`https://pokeapi.co/api/v2/type/8/`)
+    .then(res => res.json())
+    .then(response=>{
+        fetch(`${response.pokemon[getRandomInt(76)].pokemon.url}`)
+            .then(res => res.json())
+            .then(response=>{
+                 pokemonImage.innerHTML= `
+                    <img class="pokemon--sprite" src="${response.sprites.front_default}" />
+                    <h1 class="pokemon--sprite--name">${response.forms[0].name}</h1>
+                    `
+        })
+        
+    })
+});
+
+
+
+
 
 //to get a description, we need to append the resource_uri to the end of our fetch url
+// https://pokeapi.co/api/v2/type/ghost  THIS WILL GET ALL GHOST POKEMON

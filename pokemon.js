3a040d0e9f1698df249cbeebdc91965f64b1a092
Kevin <kevinalexanderduran8@gmail.com>
2021-05-12 04:34:23 -0700

    test

--- pokemon.js
+++ pokemon.js
@@ -1,41 +1,47 @@
+//set any variables you will need
 let pokemonImage = document.querySelector('.pokemon--image');
 let pokemonDesc = document.querySelector('.pokemon--desc');
 let pokedexStart = document.querySelector('.pokedex--start');
 
-function getRandomInt(max) {
-    return Math.floor(Math.random() * max);
-  }
+//create function for returning a random number below a preset value
+function getRandomInt(max){
+    return Math.floor(Math.random()*max);
+}
 
-
+//on  button click
 pokedexStart.addEventListener('click',()=>{
-    console.log(getRandomInt(76));
+
+    //we create a random number 
+    var random_poke = getRandomInt(76)
+   //we need to start with a list of all ghost pokemon
+    //fetch every ghost-type pokemon list from this url
     fetch(`https://pokeapi.co/api/v2/type/8/`)
+    //convert fetched data into readable json
     .then(res => res.json())
+    //list of every pokemon in an array containing pokemon objects
+    //use fetch to go deeper into the object
     .then(response=>{
-        fetch(`${response.pokemon[getRandomInt(76)].pokemon.url}`)
+        // we fetch a random pokemon using our random variable
+        fetch(`${response.pokemon[random_poke].pokemon.url}`)
             .then(res => res.json())
+            //now you have access to the pokemon descriptions
+            //you just need to fetch them
             .then(response=>{
-                 pokemonImage.innerHTML= `
+                //fetching into json for pokemon descriptions
+                fetch(`${response.species.url}`)
+                    .then(res => res.json())
+                    .then(response =>{ 
+                        //insert desc data into <p>
+                        //ERROR need to make sure our flavor text entries are all english
+                        pokemonDesc.innerHTML = `
+                            <p class="pokemon--desc--text">${response.flavor_text_entries[3].flavor_text}</p>
+                        `                    
+                    })
+                pokemonImage.innerHTML= `
                     <img class="pokemon--sprite" src="${response.sprites.front_default}" />
                     <h1 class="pokemon--sprite--name">${response.forms[0].name}</h1>
-                    `   
-        })
-        fetch(`https://pokeapi.co/api/v2/pokemon-species/${getRandomInt(76)}/`)
-        .then(response=>response.json())
-        .then(res=>
-            fetch(`https://pokeapi.co/api/v2/pokemon-species/${getRandomInt(76)}/`)
-                .then(response=>response.json())
-                .then(res=>
-                pokemonDesc.innerHTML= `
-                        <p class="pokemon--desc--text">${res.flavor_text_entries[1].flavor_text}</p>
                     `
-                )      
-        )        
+            })            
     })
-});
+})
 
-
-
-
-
-//to get a description, we need to append the resource_uri to the end of our fetch url

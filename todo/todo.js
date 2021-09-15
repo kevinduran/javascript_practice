e18c597870da2a632d6635e3aecf5977b8fdfda1
Kevin <kevinalexanderduran8@gmail.com>
2021-09-15 02:23:59 -0700

    test

--- todo/todo.js
+++ todo/todo.js
@@ -1,15 +1,28 @@
 const add = document.querySelector('.input-add');
 const input = document.querySelector('.input-field');
+const output = document.querySelector('.todo__container--output');
 const close_task = document.querySelector('.item-x');
 
 
 
 const getInputValue = () => {
-    console.log(input.value)
-    input.value = '';
+    console.log(input.value);
+    return input.value
+}
+
+const produceOutputValue = () => {
+    var para = document.createElement('P');
+    para.className = 'item-desc';
+    para.innerText= getInputValue();
+    console.log(para);    
+    
 }
 
 
 
+add.addEventListener('click', produceOutputValue);
 
-add.addEventListener('click', getInputValue);
+
+
+
+

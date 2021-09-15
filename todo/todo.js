0956da61e50b71dbf0a41fbe11d8d71c3abb3d0c
Kevin <kevinalexanderduran8@gmail.com>
2021-09-14 21:34:43 -0700

    JS:
    - brainstorm js strategy before code comence

--- todo/todo.js
+++ todo/todo.js
@@ -1,7 +1,15 @@
 const add = document.querySelector('.input-add');
 const input = document.querySelector('.input-field');
-
 const close_task = document.querySelector('.item-x');
 
 
-//
+
+const getInputValue = () => {
+    console.log(input.value)
+    input.value = '';
+}
+
+
+
+
+add.addEventListener('click', getInputValue);
